59e21444b0309efda9c787902915af52ad502cda
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-05-06 13:57:34 +0100

    Remove debugging

--- app/scripts/services/trackService.js
+++ app/scripts/services/trackService.js
@@ -15,15 +15,12 @@ angular.module('forzaLeagueApp')
       raceReports.$on('loaded', function(raceReports) {
         for (var raceReport in raceReports) {
           tracksUsed.push(raceReports[raceReport].track);
-          console.log(tracksUsed);
         }
       });
 
       raceReports.$on('loaded', function() {
         tracks.$on('loaded', function(tracks) {
           for (var track in tracks) {
-            console.log(tracks[track].id);
-
             if (tracksUsed.indexOf(tracks[track].id) === -1) {
               tracksNotUsed.push(tracks[track]);
             }
