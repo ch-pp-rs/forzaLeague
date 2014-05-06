93f7ec7912256a8fea2bde6525db13bc73a8556a
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-05-06 13:51:33 +0100

    Return unused tracks.

--- app/scripts/services/trackService.js
+++ app/scripts/services/trackService.js
@@ -1,12 +1,36 @@
 'use strict';
 
 angular.module('forzaLeagueApp')
-  .service('trackService', function($firebase) {
+  .service('trackService', function($firebase, raceReportService) {
     this.getTracks = function () {
       return $firebase(new Firebase('https://forza.firebaseio.com/tracks'));
     };
 
     this.getUnusedTracks = function () {
-      return $firebase(new Firebase('https://forza.firebaseio.com/tracks'));
+      var tracksUsed = [],
+          tracksNotUsed = [],
+          raceReports = raceReportService.getRaceReports(),
+          tracks = $firebase(new Firebase('https://forza.firebaseio.com/tracks'));
+
+      raceReports.$on('loaded', function(raceReports) {
+        for (var raceReport in raceReports) {
+          tracksUsed.push(raceReports[raceReport].track);
+          console.log(tracksUsed);
+        }
+      });
+
+      raceReports.$on('loaded', function() {
+        tracks.$on('loaded', function(tracks) {
+          for (var track in tracks) {
+            console.log(tracks[track].id);
+
+            if (tracksUsed.indexOf(tracks[track].id) === -1) {
+              tracksNotUsed.push(tracks[track]);
+            }
+          }
+        });
+      });
+
+      return tracksNotUsed;
     };
   });
